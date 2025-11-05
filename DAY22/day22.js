2e54eceef2ba2bf33c03eb699f02096f63d4dba3
Batta6 <battamanojkumarnaidu@gmail.com>
2025-11-05 16:17:02 +0530

    day22

---
+++ DAY22/day22.js
@@ -0,0 +1,20 @@
+let variable=1;
+let name="Manoj";
+const batta="sun rises in east only";
+let town;
+name="B.Manoj";
+console.log(name);
+console.log(variable);
+let isActive = true; 
+let x; 
+let a = null; 
+let names = ["manoj", "indu", "lalli", "adhi","aswi"]; 
+let person = { 
+    name: "manoj", 
+    company: "oppty", 
+    year: 2025
+}; 
+
+document.getElementById("hi").innerHTML =names;
+document.getElementsByClassName("hello").innerHTML=names(0);
+console.log(person); 
